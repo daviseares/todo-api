f080fba9271404eb80babacc6ae6548dd2f6413d
davi.borges <davi.borges@codificar.com.br>
2020-05-14 19:08:09 -0300

    up project

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    public function index(){
+        return Task::all();
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'name'=>'required|max:255',
+            'complete'=>'required'
+        ]);
+
+        $task = Task::create([
+            'name'=>$request->input('name'),
+            'complete'=>$request->input('complete')
+        ]);     
+          
+        $response=[
+            "success"=>true,
+            "msg"=>"Nova tarefa criada!",
+            "data"=>$task
+        ];
+        
+        return $response;
+    }
+
+    public function show(Task $task){
+        
+        return $task;
+
+    }
+
+    public function update(Request $request,Task $task){
+
+    $request->validate([
+        'name'=>'required|max:255',
+            
+    ]);
+
+     $task->name = $request -> input('name');
+     $task->save();
+     
+     $response=[
+        "success"=>true,
+        "msg"=>"Tarefa atualizada!",
+        "data"=>$task
+    ];
+    
+    return $response;
+    
+    }
+
+    public function destroy(Task $task){
+            
+        $task->delete();
+        
+        $response=[
+           "success"=>true,
+           "msg"=>"Tarefa excluída!"
+       ];
+       
+       return $response;
+       
+    }
+}
